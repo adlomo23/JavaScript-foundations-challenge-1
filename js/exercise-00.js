21360eea7718d0b02ae1994cd80e1f7602fc7047
adlomo23 <adlomo23@gmail.com>
2023-10-17 20:25:38 +0200

    Finished exercise-00

---
+++ js/exercise-00.js
@@ -0,0 +1,24 @@
+/* 
+   Create a constant variable of each of the following primitive types:
+   - String
+   - Number
+   - Boolean
+   - Null
+   - Undefined
+
+   Log out each variable to the terminal at the end.
+*/
+
+const varString = "Hola Jaume";
+const varNumber = 23;
+const varBoolean = true;
+const varNull = null;
+const varUndefined = undefined;
+
+
+console.log({varString});
+console.log({varNumber});
+console.log({varBoolean});
+console.log({varNull});
+console.log({varUndefined});
+
