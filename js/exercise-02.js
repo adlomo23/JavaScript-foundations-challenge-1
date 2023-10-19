e175ff115f9565fca06f345f4498d7d28ef4ad0f
adlomo23 <adlomo23@gmail.com>
2023-10-19 15:21:06 +0200

    Finished ex_02

---
+++ js/exercise-02.js
@@ -0,0 +1,28 @@
+/* 
+    1. Declare a variable using let with any name you like, but without a value.
+    2. On a separate line, assign the variable the String: "Moo!"
+    3. Log out this variable to the terminal using console.log
+    
+    Repeat the above steps but try using a const variable. Comment this new part of code and explain what is happening.
+*/
+
+
+let message;
+
+
+message = "Moo!";
+
+
+console.log(message);
+
+// We declare a variable named message using let without assigning a value initially.
+// We assign the string "Moo!" to the message variable.
+// We use console.log to log the value of message to the terminal, which will display "Moo!".
+
+// const message2;
+
+message2 = "Moo";
+
+console.log(message2);
+
+// Now it gives us an error since a constant must be initialized from the beginning
