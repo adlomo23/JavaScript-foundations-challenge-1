fb30d7847c9dfe524a94a5ffaf7f71dac0621b6a
adlomo23 <adlomo23@gmail.com>
2023-11-05 17:02:41 +0100

    finished ex0 conditionals

--- challenge_03_loops-conditionals/03_JavaScript_foundations/loops-conditionals/conditionals/exercise_00.js
+++ challenge_03_loops-conditionals/03_JavaScript_foundations/loops-conditionals/conditionals/exercise_00.js
@@ -8,3 +8,16 @@
     7. At the end of the program print out: "--- Animal check complete ---"
     8. Change animal to different values to make sure it works in all cases
 */
+
+console.log("--- Animal Checker Program ---")
+var animal = "cat";
+
+if(anime === "cat"){
+    console.log("Meow!");
+} else if(animal === "dog"){
+    console.log("Woof!");
+} else{
+    console.log("Must be an alien");
+}
+
+console.log("--- Animal check complete ---");
